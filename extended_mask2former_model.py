79ab272a39f0e14efbdf81ab0912708e7f207d2c
Stamatis Orfanos <stamatisorfanos99@gmail.com>
2024-03-01 01:54:18 +0200

    Final Mask2Former test

--- extended_mask2former_model.py
+++ extended_mask2former_model.py
@@ -6,28 +6,25 @@ from models.mask2former_detector.mask2former_model import Mask2Former
 
 
 class ExtendedMask2Former(nn.Module):
-    """ The class ExtendedMask2Former is our implementation of the Maks2Former model as a detector and the 
-        EFPN model as a backbone and a feature map creator.
+    """
+    Extended Mask Transformer Model integrating EFPN as the backbone and feature mask generator, with Mask2Former for instance segmentation tasks.
+    The model uses the Enhanced Feature Pyramid Network (EFPN) to extract multi-scale feature maps and generate corresponding mask features 
+    from images. These features are then used by the Mask2Former model to perform instance segmentation, identifying and delineating each object 
+    instance in the input image.
 
-    Args:
-        num_classes()     : 
-        hidden_dim()      :256, 
-        num_queries()     : 100, 
-        nheads()          : 8, 
-        dim_feedforward() : , 
-        dec_layers()      :=1, 
-        mask_dim          :()=256
+    Parameters:
+        efpn (EFPN): The Enhanced Feature Pyramid Network model used as the backbone for feature and mask feature extraction.
+        mask2former (Mask2Former): The Mask2Former model used for predicting object instances and their masks based on the features provided by EFPN.
     """
     def __init__(self, num_classes, hidden_dim=256, num_queries=100, nheads=8, dim_feedforward=2048, dec_layers=1, mask_dim=100):
-        super(ExtendedMask2Former, self).__init__()
-                
-        self.backbone = EFPN(hidden_dim, hidden_dim, mask_dim)
-        self.decoder = Mask2Former(hidden_dim, num_classes, hidden_dim, num_queries, nheads, dim_feedforward, dec_layers, mask_dim)
+        super(ExtendedMask2Former, self).__init__()              
+        self.efpn = EFPN(hidden_dim, hidden_dim, mask_dim)
+        self.mask2former = Mask2Former(hidden_dim, num_classes, hidden_dim, num_queries, nheads, dim_feedforward, dec_layers, mask_dim)
         
         
     def forward(self, image, hidden_dim):
-        feature_maps, masks = self.backbone(image, hidden_dim)
-        output = self.decoder(feature_maps, masks)
+        feature_maps, masks = self.efpn(image, hidden_dim)
+        output = self.mask2former(feature_maps, masks)
         return output
     
     
